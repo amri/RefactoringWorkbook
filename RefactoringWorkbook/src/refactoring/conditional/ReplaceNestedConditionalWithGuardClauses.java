e01bfbaa6d1c6f83535ec3319de79b73dfdbc650
Amri Hidayat <amri.hidayat@gmail.com>
2014-12-26 21:14:33 +0800

    AFTER - Replace Nested Conditional with Guard Clauses - Reversing the Conditions

--- RefactoringWorkbook/src/refactoring/conditional/ReplaceNestedConditionalWithGuardClauses.java
+++ RefactoringWorkbook/src/refactoring/conditional/ReplaceNestedConditionalWithGuardClauses.java
@@ -22,13 +22,10 @@ public class ReplaceNestedConditionalWithGuardClauses {
 	
 	double getAdjustedCapital()
 	{
-		double result = 0.0;
-		if(_capital > 0.0) {
-			if(_intRate > 0.0 && _duration > 0.0) {
-				result = (_income / _duration) * ADJ_FACTOR;
-			}
-		}
-		return result;
+		if(_capital <= 0.0) return 0.0;
+		if(_intRate <= 0.0 || _duration <= 0.0) return 0.0;
+		return (_income / _duration) * ADJ_FACTOR;
+		
 	}
 
 	private double retiredAmount() {
