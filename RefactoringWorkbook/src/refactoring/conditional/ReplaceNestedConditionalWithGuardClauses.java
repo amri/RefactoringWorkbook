c0d1e1ca45f2bb0b12d1f2b6fac3f154e457b09b
Amri Hidayat <amri.hidayat@gmail.com>
2014-12-26 20:57:37 +0800

    AFTER - Replace Nested Conditional with Guard Clauses

--- RefactoringWorkbook/src/refactoring/conditional/ReplaceNestedConditionalWithGuardClauses.java
+++ RefactoringWorkbook/src/refactoring/conditional/ReplaceNestedConditionalWithGuardClauses.java
@@ -8,15 +8,11 @@ public class ReplaceNestedConditionalWithGuardClauses {
 	double getPayAmount()
 	{
 		double result;
-		if(_isDead) result = deadAmount();
-		else {
-			if (_isSeparated) result = separatedAmount();
-			else {
-				if (_isRetired) result = retiredAmount();
-				else result = normalPayAmount();
-			}
-		}
-		return result;
+		if(_isDead) return deadAmount();
+		if (_isSeparated) return separatedAmount();
+		if (_isRetired) return retiredAmount();
+
+		return normalPayAmount();
 	}
 
 	private double retiredAmount() {
